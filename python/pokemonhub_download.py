6f76bed441283850d3ff541b8d85970a7b423df5
James Zaman <jzaman182040@bscse.uiu.ac.bd>
2021-07-20 21:37:53 +0600

    Pokemonhub also comes from server.

--- python/pokemonhub_download.py
+++ python/pokemonhub_download.py
@@ -1,57 +1,41 @@
 import requests
 import json
+from urllib import request
 from os import environ, listdir, mkdir
 
 # User defined modules.
 from download import download_file
 
-def pokemonhub_download(urls):
-  """Give the link to any pornhub video and that video will be downloaded to your google drive Colab_Files/hub/ folder"""
-  if type(urls) is not list:
-    urls = [urls]
-
-  # Making post requests
-
-  # Variable declarations.
-  urlFetcherDomain = 'https://xxxsave.net'
-  urlFetcher = 'https://xxxsave.net/result-fetch'
-
-  # Setting up client.
-  client = requests.session()
-  client.get(urlFetcherDomain)
-  csrftoken = client.cookies['csrftoken']
-
-  # Data to be sent.
-  for url in urls:
-    login_data = {
-        'csrfmiddlewaretoken': csrftoken,
-        'url': url,
-    }
-
-    # Fetching the url and converting it.
-    urlData = json.loads(client.post(urlFetcher, data = login_data, headers={'Referer' : urlFetcher}).text)
+def pokemonhub_download(url):
+  # converting url to fetching url
+  url = f'https://anidownserver.jameshedayet.repl.co/getpokemonhuburl?pokemon={url}'
+  # Fetching the url and converting it.
+  user_agent = 'Mozilla/5.0'
+  urlData = request.urlopen(request.Request(f'{url}', data=None, headers={'User-Agent': user_agent})).read().decode()
+  # Converting string to dict.
+  urlData = json.loads(urlData)
+  
+  if urlData['success'] == 'true':
+    title = urlData['title']
     
-    if urlData['success'] == 'true':
-      title = urlData['title']
-      
-      # Sanatize title
-      title = title.replace('\n', '')
-      title = title.replace('\t', '')
-      title = title.replace('/', '|')
-      title = title.replace('\\', '|')
-      
+    # Sanatize title
+    title = title.replace('\n', '')
+    title = title.replace('\t', '')
+    title = title.replace('/', '|')
+    title = title.replace('\\', '|')
+    
 
-      # Making sure the Downloads folder exists.
-      target_folder = f'C:{environ["HOMEPATH"]}'
-      dir_list = listdir(target_folder)
-      if 'Downloads' not in dir_list:
-        mkdir(f'{target_folder}/Downloads')
-      target_folder += '/Downloads'
-      # Making sure the Anidown folder exists.
-      dir_list = listdir(target_folder)
-      if 'Anidown' not in dir_list:
-        mkdir(f'{target_folder}/Anidown')
-      target_folder += '/Anidown'
-      # Downloading the video.
-      quality = urlData['quality'][-1]
-      download_file(urlData[f's{quality}'], f'{target_folder}/{title}.mp4')
+    # Making sure the Downloads folder exists.
+    target_folder = f'C:{environ["HOMEPATH"]}'
+    dir_list = listdir(target_folder)
+    if 'Downloads' not in dir_list:
+      mkdir(f'{target_folder}/Downloads')
+    target_folder += '/Downloads'
+    # Making sure the Anidown folder exists.
+    dir_list = listdir(target_folder)
+    if 'Anidown' not in dir_list:
+      mkdir(f'{target_folder}/Anidown')
+    target_folder += '/Anidown'
+    # Downloading the video.
+    quality = urlData['quality'][-1]
+    download_file(urlData[f's{quality}'], f'{target_folder}/{title}.mp4')
