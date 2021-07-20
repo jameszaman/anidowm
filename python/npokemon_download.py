82342cd0acbd200edc1007f2b48c52d65870f645
James Zaman <jzaman182040@bscse.uiu.ac.bd>
2021-07-20 19:56:21 +0600

    Deleted unnecessary comments.

--- python/npokemon_download.py
+++ python/npokemon_download.py
@@ -5,15 +5,6 @@ from os import environ, listdir, mkdir
 from download import download_file
 
 def npokemon_download(name, urls):
-  # ------------------------------------
-  # # Destination where to save the videos.
-  # target_folder = environ["HOMEPATH"]
-  # # Making sure the Downloads folder exists.
-  # dir_list = listdir(target_folder)
-  # if 'Downloads' not in dir_list:
-  #   mkdir(f'{target_folder}/Downloads')
-  # target_folder = f'C:{target_folder}\\Downloads'
-  # ------------------------------------
   # Making sure the download folder exists.
   target_folder = f'C:{environ["HOMEPATH"]}'
   # Making sure Downloads folder exists.
