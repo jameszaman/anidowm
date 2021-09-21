b52b5e509d7fb7addff3825a1995e957a11474a3
James Zaman <jzaman182040@bscse.uiu.ac.bd>
2021-09-22 01:49:20 +0600

    Using the nav button to toggle sidebar.

---
+++ src/js/nav.js
@@ -0,0 +1,13 @@
+// Selecting elements.
+const dropdownButton = document.querySelector('#dropdown-button');
+const navDropdown = document.querySelector("#nav-dropdown");
+
+// Events
+dropdownButton.addEventListener('click', () => {
+  if(navDropdown.classList.contains('hidden')) {
+    navDropdown.classList.remove('hidden');
+  }
+  else {
+    navDropdown.classList.add("hidden");
+  }
+});
