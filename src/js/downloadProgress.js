2505a46324c2d210dde74ba8994076e0e3f7d8e9
James Zaman <jzaman182040@bscse.uiu.ac.bd>
2021-09-21 23:54:29 +0600

    Any new download is now added to the progressContainer to show downloading.

--- src/js/downloadProgress.js
+++ src/js/downloadProgress.js
@@ -39,8 +39,11 @@ async function addToProgressbar(name, url) {
   const data = await axios.head(url);
   const size = data.headers["content-length"];
 
-  // Saving in name: url status size
-  localStorage.setItem(name, `${url} downloading ${size}`);
+  // Saving in localstorage. name: url size
+  localStorage.setItem(name, `${url} ${size}`);
+
+  // Adding it to progressContainer to show it downloading.
+  progressContainer.downloading.push({ name, size });
 }
 
 function showProgressbar() {
@@ -51,14 +54,13 @@ function showProgressbar() {
   showProgressId = setInterval(() => {
     progressContainer.downloading.forEach((progress) => {
       // Extracting data about all the downloads.
-      const progressSplitted = progress.url.split(" ");
       const locationSplit = progress.name.split("/");
       const name = locationSplit[locationSplit.length - 1];
       // Getting the size of how much downloaded.
       const stat = fs.statSync(progress.name);
-      const downloadProgress = (stat.size/progressSplitted[2] * 100).toFixed(3);
+      const downloadProgress = (stat.size / progress.size * 100).toFixed(3);
       // Stop showing the output when it reaches 100%
-      if(downloadProgress > 99) {
+      if(downloadProgress === '100.000') {
         progressContainer.downloaded.push(progress);
         // Removing the element.
         progressContainer.downloading = progressContainer.downloading.filter(prog => prog != progress);
