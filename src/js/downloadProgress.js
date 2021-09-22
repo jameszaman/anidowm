4e117b0dd155a8e449bb2db74bcd379333e58501
James Zaman <jzaman182040@bscse.uiu.ac.bd>
2021-09-22 08:39:29 +0600

    Already stored download information now showing on download progress bar

--- src/js/downloadProgress.js
+++ src/js/downloadProgress.js
@@ -3,33 +3,44 @@ const axios = require('axios');
 const fs = require('fs');
 
 // Necessary variables.
-let progressContainer = { downloading: [], downloaded: [], incomplete: [], deleted: [] };
+let progressContainer = { downloading: [], incomplete: [], deleted: [] };
 let showProgressId = 0;
+
+function showInProgressbar(name, cssClass) {
+  const progressBar = document.createElement("p");
+  if(cssClass === "downloading") {
+    progressBar.innerText = `${name}: 0% completed.`;
+  }
+  else {
+    progressBar.innerText = name;
+  }
+  progressBar.classList.add(cssClass);
+  navDropdown.appendChild(progressBar);
+  return progressBar;
+}
 
 // Turning the localStorage into an array.
 const keys = Object.keys(localStorage);
 keys.forEach(key => {
+  // Extracting the name.
+  const keySplit = key.split('/');
+  const name = keySplit[keySplit.length - 1];
+  console.log(key, name);
+  
   try {
+    // Getting size.
     const stat = fs.statSync(key);
     const itemSize = localStorage.getItem(key).split(' ')[1];
+
     if(stat.size == itemSize) {
-      progressContainer.downloaded.push({
-        name: key,
-        url: localStorage.getItem(key),
-      });
+      showInProgressbar(name, 'downloaded');
     }
     else {
-      progressContainer.incomplete.push({
-        name: key,
-        url: localStorage.getItem(key),
-      });
+      showInProgressbar(name, "incomplete");
     }
   }
   catch(e) {
-    progressContainer.deleted.push({
-      name: key,
-      url: localStorage.getItem(key),
-    });
+    showInProgressbar(name, "deleted");
   }
 });
 
@@ -42,32 +53,35 @@ async function addToProgressbar(name, url) {
   // Saving in localstorage. name: url size
   localStorage.setItem(name, `${url} ${size}`);
 
-  // Adding it to progressContainer to show it downloading.
-  progressContainer.downloading.push({ name, size });
+
+  // Adding it to navDropdown and progressContainer to show it downloading.
+  const downloadProgress = showInProgressbar(name, "downloading");
+  progressContainer.downloading.push({ name, downloadProgress, size });
 }
 
-function showProgressbar() {
+function showProgress(container) {
   // making sure progressbar is not alredy showing.
   clearInterval(showProgressId);
   
-  // Showing progress.
+  
+  // Update progress.
   showProgressId = setInterval(() => {
     progressContainer.downloading.forEach((progress) => {
-      // Extracting data about all the downloads.
-      const locationSplit = progress.name.split("/");
-      const name = locationSplit[locationSplit.length - 1];
       // Getting the size of how much downloaded.
       const stat = fs.statSync(progress.name);
       const downloadProgress = (stat.size / progress.size * 100).toFixed(3);
+
       // Stop showing the output when it reaches 100%
       if(downloadProgress === '100.000') {
-        progressContainer.downloaded.push(progress);
+        // Showing it as complete.
+        progressContainer.downloaded.classList.remove('downloading');
+        progressContainer.downloaded.classList.add('downloaded');
         // Removing the element.
         progressContainer.downloading = progressContainer.downloading.filter(prog => prog != progress);
       }
       else {
         // Showing the output.
-        console.log(`${name}: ${downloadProgress}% completed.`);
+        progress.downloadProgress.innerText = `${progress.name}: ${downloadProgress}% completed.`;
       }
     });
   }, 5000);
--- src/js/nav.js
+++ src/js/nav.js
@@ -2,6 +2,7 @@
 const dropdownButton = document.querySelector('#dropdown-button');
 const navDropdown = document.querySelector("#nav-dropdown");
 
+
 // Events
 dropdownButton.addEventListener('click', () => {
   if(navDropdown.classList.contains('hidden')) {
