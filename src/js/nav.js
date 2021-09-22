74017677a57679a68ed9f5e1fdca9545c5d7f686
James Zaman <jzaman182040@bscse.uiu.ac.bd>
2021-09-22 10:54:28 +0600

    Sidebar working properly now.

--- src/js/nav.js
+++ src/js/nav.js
@@ -5,12 +5,12 @@ const navDropdown = document.querySelector("#nav-dropdown");
 
 // Events
 dropdownButton.addEventListener('click', () => {
-  if(navDropdown.classList.contains('hidden')) {
-    navDropdown.classList.remove('hidden');
+  if(navDropdown.classList.contains('nav-dropdown-hidden')) {
+    navDropdown.classList.remove('nav-dropdown-hidden');
     showProgress();
   }
   else {
-    navDropdown.classList.add("hidden");
+    navDropdown.classList.add("nav-dropdown-hidden");
     stopShowingProgress();
   }
 });
