94cbf87c79fd0d315838be741d1e0d67de1a5302
James Zaman <jzaman182040@bscse.uiu.ac.bd>
2021-10-24 02:26:15 +0600

    There is a bit of delay in between npokemon page downloads. This is to make sure all downloads are not starting at the same time as our PC can't handle that.

--- src/js/npokemonDownload.js
+++ src/js/npokemonDownload.js
@@ -2,6 +2,10 @@
 const user_agent =
   "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:90.0) Gecko/20100101 Firefox/90.0";
 
+// This variables controls how long we will wait before starting download.
+// This is used to make sure too many pages are not starting download together.
+let waitTimer = 1;
+
 async function downloadNPokemon(name, urls) {
   // Shortening long names.
   if (name.length > 150) {
@@ -14,11 +18,14 @@ async function downloadNPokemon(name, urls) {
   // Creating a folder to store the manga.
   const targetFolder = makeFolder(name);
   // Downloading all the pages.
-  for (const url of urls) {
-    // Getting the page no.
-    let page = url.split("/");
-    page = page[page.length - 1];
-    // Downloading the page.
-    download(url, `${targetFolder}/${page}`);
+  for (const index in urls) {
+    setTimeout(() => {
+      // Getting the page no.
+      let page = urls[index].split("/");
+      page = page[page.length - 1];
+      // Downloading the page.
+      download(urls[index], `${targetFolder}/${page}`);
+    }, waitTimer);
+    waitTimer += 500;
   }
 }
