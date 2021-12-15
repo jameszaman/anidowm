37e991b6b330e533daed80493878d2e9b533acfa
James Zaman <jzaman182040@bscse.uiu.ac.bd>
2021-12-15 19:49:17 +0600

    Making sure that if a file is already downloaded not to download it again.

--- src/js/download.js
+++ src/js/download.js
@@ -3,23 +3,38 @@ const path = require('path');
 const { spawn } = require('child_process');
 
 
-function download(url, name) {
-  // This is the path for python download file.
-  // Need different path for development and production.
-  // *** This needs to be inside the function,***
-  // *** or global.appPath is undefined as app was not initialized yet. ***
-  if (process.env.DEVELOPMENT) {
-    downloader = "python/download.py";
-  } else {
-    downloader = path.join(global.appPath, "..", "python/download.py");
-  }
+async function download(url, filePath) {
+  // We should not download the file if it already exists.
+  // Extracting the file path.
+  const fileDir = filePath.slice(
+    0,
+    Math.max(filePath.lastIndexOf("\\"), filePath.lastIndexOf("/"))
+  );
+  // Checking if a file already exists.
+  const files = await fs.readdirSync(fileDir);
+  const filePathSplit = filePath.split(/[\/\\]/gi);
+  const fileAlreadyExists = files.includes(
+    filePathSplit[filePathSplit.length - 1]
+  );
 
-  // If arguments are array, pass them in array format for python.
-  if (Array.isArray(url)) {
-    url = `[${url.toString()}]`;
-    name = `[${name.toString()}]`;
-  }
+  if (!fileAlreadyExists) {
+    // This is the path for python download file.
+    // Need different path for development and production.
+    // *** This needs to be inside the function,***
+    // *** or global.appPath is undefined as app was not initialized yet. ***
+    if (process.env.DEVELOPMENT) {
+      downloader = "python/download.py";
+    } else {
+      downloader = path.join(global.appPath, "..", "python/download.py");
+    }
 
-  // Downloading the file.
-  spawn("python", [downloader, url, name]);
+    // If arguments are array, pass them in array format for python.
+    if (Array.isArray(url)) {
+      url = `[${url.toString()}]`;
+      filePath = `[${filePath.toString()}]`;
+    }
+
+    // Downloading the file.
+    spawn("python", [downloader, url, filePath]);
+  }
 };
