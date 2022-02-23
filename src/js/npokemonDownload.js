21a911170225051a52e02dae47a3b33561106b0e
James Zaman <jzaman182040@bscse.uiu.ac.bd>
2022-02-24 00:06:12 +0600

    As we are not using python anymore, NPokemon does not need a delay.

--- src/js/npokemonDownload.js
+++ src/js/npokemonDownload.js
@@ -1,7 +1,3 @@
-// This variables controls how long we will wait before starting download.
-// This is used to make sure too many pages are not starting download together.
-let waitTimer = 1;
-
 async function downloadNPokemon(name, urls) {
   // Shortening long names.
   if (name.length > 150) {
@@ -12,13 +8,10 @@ async function downloadNPokemon(name, urls) {
   const targetFolder = makeFolder(name);
   // Downloading all the pages.
   for (const index in urls) {
-    setTimeout(() => {
-      // Getting the page no.
-      let page = urls[index].split("/");
-      page = page[page.length - 1];
-      // Downloading the page.
-      download(urls[index], `${targetFolder}/${page}`);
-    }, waitTimer);
-    waitTimer += 500;
+    // Getting the page no.
+    let page = urls[index].split("/");
+    page = page[page.length - 1];
+    // Downloading the page.
+    download(urls[index], `${targetFolder}/${page}`);
   }
 }
