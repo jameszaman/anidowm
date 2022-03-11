6e6e973e19c9049c93a3b5e5636fbb1f6dcafeb7
James Zaman <jzaman182040@bscse.uiu.ac.bd>
2022-03-11 16:35:44 +0600

    backing up before changing OS.

--- src/js/npokemonDownload.js
+++ src/js/npokemonDownload.js
@@ -1,3 +1,7 @@
+// This variables controls how long we will wait before starting download.
+// This is used to make sure too many pages are not starting download together.
+let waitTimer = 1;
+
 async function downloadNPokemon(name, urls) {
   // Shortening long names.
   if (name.length > 150) {
@@ -8,10 +12,13 @@ async function downloadNPokemon(name, urls) {
   const targetFolder = makeFolder(name);
   // Downloading all the pages.
   for (const index in urls) {
-    // Getting the page no.
-    let page = urls[index].split("/");
-    page = page[page.length - 1];
-    // Downloading the page.
-    download(urls[index], `${targetFolder}/${page}`);
+    setTimeout(() => {
+      // Getting the page no.
+      let page = urls[index].split("/");
+      page = page[page.length - 1];
+      // Downloading the page.
+      download(urls[index], `${targetFolder}/${page}`);
+    }, waitTimer);
+    waitTimer += 750;
   }
 }
